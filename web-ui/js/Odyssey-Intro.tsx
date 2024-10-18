38c7a48c30b898218c97143f717d8fb0545a3622
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-18 08:42:20 -0700

    simplify

--- web-ui/js/Odyssey-Intro.tsx
+++ web-ui/js/Odyssey-Intro.tsx
@@ -121,7 +121,7 @@ export default function Example() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-8 sm:py-12 lg:py-16">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
 
 
@@ -139,14 +139,14 @@ export default function Example() {
 
      
           <div className="text-center">
-            <h1 className="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-balance text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Thanks to BotParty.org + Dynamicland.org!
             </h1>
             {/* <p className="mt-6 text-lg leading-8 text-gray-600">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
             </p> */}
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="flex items-center justify-center gap-x-6">
               {/* <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
